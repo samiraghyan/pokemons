23f131db6a4ece3af8a3bd9719dc4a6b142409bd
Sveta <svetamiraghyan@gmail.com>
2019-04-01 12:37:41 +0400

    Updates

---
+++ precache-manifest.42c5f6e931a83547f9a1b89640507e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f18731efbf56bce2669",
+    "url": "/website/static/css/main.988013ec.chunk.css"
+  },
+  {
+    "revision": "0f18731efbf56bce2669",
+    "url": "/website/static/js/main.a73e1c23.chunk.js"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.0439b053.js"
+  },
+  {
+    "revision": "d7ba4ab88da05cf9ab09",
+    "url": "/website/static/js/2.7825989c.chunk.js"
+  },
+  {
+    "revision": "6a1a783c610b620abce76207f1e1967e",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.f811e835dfd9c38fb2dfafc50878bdb8.js"
+  "/website/precache-manifest.42c5f6e931a83547f9a1b89640507e34.js"
 );
 
 workbox.clientsClaim();
