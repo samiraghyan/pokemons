7db08cf34edb710d9342ba14275dfe92ba724aed
armenharutyunyanbills <aharutyunyan@bills.com>
2019-09-02 20:58:45 +0400

    Updates

--- precache-manifest.cb0cd193942754712a27da32a7fea17f.js
+++ precache-manifest.cb0cd193942754712a27da32a7fea17f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/website/static/js/runtime~main.0439b053.js"
   },
   {
-    "revision": "4839da238abb73694ce1",
-    "url": "/website/static/js/main.45ea238e.chunk.js"
+    "revision": "63c88bba259d7b0634fb",
+    "url": "/website/static/js/main.3fedcd01.chunk.js"
   },
   {
     "revision": "d575e24fdadfd5c51ceb",
     "url": "/website/static/js/2.0269f04b.chunk.js"
   },
   {
-    "revision": "4839da238abb73694ce1",
+    "revision": "63c88bba259d7b0634fb",
     "url": "/website/static/css/main.d599de59.chunk.css"
   },
   {
-    "revision": "0cbc3c44d96bab647f5d2ddb4156262b",
+    "revision": "f873b0818f15ba4918888c4f86a98047",
     "url": "/website/index.html"
   }
 ];
