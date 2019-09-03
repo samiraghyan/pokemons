67b477fc6a8b2f08aaab2890a67ea46230e9a283
armenharutyunyanbills <aharutyunyan@bills.com>
2019-09-03 19:37:13 +0400

    Updates

---
+++ precache-manifest.eb76fb24bf8ad9e0ac4a5333f7a358d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5d4841f8e00298f7397",
+    "url": "/pokemons/static/js/runtime~main.c2a0dfef.js"
+  },
+  {
+    "revision": "75641b0388e85f590503",
+    "url": "/pokemons/static/js/main.8dcefc01.chunk.js"
+  },
+  {
+    "revision": "c1e1748acabfb7125d47",
+    "url": "/pokemons/static/js/2.e835237c.chunk.js"
+  },
+  {
+    "revision": "75641b0388e85f590503",
+    "url": "/pokemons/static/css/main.d599de59.chunk.css"
+  },
+  {
+    "revision": "48404ff96dfa7666af775557c3d73f30",
+    "url": "/pokemons/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.a6f9674497acf49ed5e0d9d2ce6578d8.js"
+  "/pokemons/precache-manifest.eb76fb24bf8ad9e0ac4a5333f7a358d0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/pokemons/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
