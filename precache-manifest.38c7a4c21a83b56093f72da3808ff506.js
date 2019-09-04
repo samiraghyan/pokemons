67fa5eca8f1fb8c4fe11b9ca2354bc9b91a40909
armenharutyunyanbills <aharutyunyan@bills.com>
2019-09-04 17:09:59 +0400

    Updates

---
+++ precache-manifest.38c7a4c21a83b56093f72da3808ff506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5d4841f8e00298f7397",
+    "url": "/pokemons/static/js/runtime~main.c2a0dfef.js"
+  },
+  {
+    "revision": "5a9012a50605f382e93d",
+    "url": "/pokemons/static/js/main.cf07b7c4.chunk.js"
+  },
+  {
+    "revision": "5f78c941445f0afcfb88",
+    "url": "/pokemons/static/js/2.48380f98.chunk.js"
+  },
+  {
+    "revision": "5a9012a50605f382e93d",
+    "url": "/pokemons/static/css/main.d599de59.chunk.css"
+  },
+  {
+    "revision": "b6025d41b203c492f283df9ce9dc4c7d",
+    "url": "/pokemons/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokemons/precache-manifest.e0e0d13308bf22394c686bcf05fcd17e.js"
+  "/pokemons/precache-manifest.38c7a4c21a83b56093f72da3808ff506.js"
 );
 
 workbox.clientsClaim();
